3ca9eb89c79ba36d9b8cf0d0ea2cd36670ec973d
Angelo Salamanca <angelosalamanca21@gmail.com>
2024-03-11 21:20:43 -0500

    Changes

--- calculator.py
+++ calculator.py
@@ -1,17 +1,17 @@
 #Suma
-def sumar(num1, num2):
+def addition(num1, num2):
     return num1 + num2
 
 #Resta
-def restar(num1, num2):
+def subtract(num1, num2):
     return num1 - num2
 
 #Multiplicación
-def multiplicar(num1, num2):
+def multiply(num1, num2):
     return num1 * num2
 
 #División
-def dividir(num1, num2):
+def division(num1, num2):
     #División por cero
     if num2 == 0:
         return "Error: No se puede dividir por cero."
@@ -27,22 +27,22 @@ while True:
     print("4. Dividir")
     print("5. Salir")
 
-    opcion = input("Ingresa el número de la operación que deseas realizar: ")
+    option = input("Ingresa el número de la operación que deseas realizar: ")
 
-    if opcion == '5':
+    if option == '5':
         print("¡Hasta luego!")
         break
 
     num1 = float(input("Ingresa el primer número: "))
     num2 = float(input("Ingresa el segundo número: "))
 
-    if opcion == '1':
-        print("Resultado:", sumar(num1, num2))
-    elif opcion == '2':
-        print("Resultado:", restar(num1, num2))
-    elif opcion == '3':
-        print("Resultado:", multiplicar(num1, num2))
-    elif opcion == '4':
-        print("Resultado:", dividir(num1, num2))
+    if option == '1':
+        print("Resultado:", addition(num1, num2))
+    elif option == '2':
+        print("Resultado:", subtract(num1, num2))
+    elif option == '3':
+        print("Resultado:", multiply(num1, num2))
+    elif option == '4':
+        print("Resultado:", division(num1, num2))
     else:
         print("Opción no válida. Por favor, selecciona una opción válida.")
